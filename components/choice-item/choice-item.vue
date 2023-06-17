309da1c014cbbad7dc016dba412ba3ced1ccc5d0
tangbingxiao <tangbingxiao@foxmail.com>
2023-06-17 14:02:18 +0800

    init

---
+++ components/choice-item/choice-item.vue
@@ -0,0 +1,41 @@
+<template>
+    <view>
+        <view class="ctx" :class="isActive ? 'active': ''">
+            {{content}}
+        </view>
+
+    </view>
+</template>
+
+<script>
+    export default {
+        name: "choice-item",
+        props: {
+            content: {
+                type: String,
+                default: ''
+            },
+            isActive: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data() {
+            return {
+
+            };
+        }
+    }
+</script>
+
+<style lang="scss">
+    .ctx {
+        font-size: 30rpx;
+        border-radius: 20rpx;
+        //border: 1px solid black;
+
+        &.active {
+            background-color: #ff0000
+        }
+    }
+</style>
---
+++ pages/questions/questions.vue
@@ -0,0 +1,57 @@
+<template>
+    <view class="main">
+        <view class="tQuestion">{{question.content}}</view>
+        <view class="choices">
+            <view v-for="choice in question.choices">
+                <view class="ch">
+                    <choice-item :content="choice" :isActive="isActive"></choice-item>
+                </view>
+            </view>
+        </view>
+        <view class="tPreButton">
+            <u-button type="primary" text="上一题"></u-button>
+        </view>
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                isActive: false,
+                question: {
+                    content: "问题",
+                    choices: [
+                        "选项1",
+                        "选项2",
+                        "选项3",
+                        "选项4"
+                    ]
+                }
+            };
+        }
+    }
+</script>
+
+<style lang="scss">
+    .main {
+        background-image: url("../../static/images/bg-img.png");
+        height: 100vh;
+
+        .tQuestion {
+            margin: 0 auto;
+            text-align: center;
+        }
+
+        .tPreButton {
+            width: 15%;
+            margin: 10rpx 10%;
+        }
+
+        .choices {
+            .ch {
+                margin: 10rpx 10%;
+            }
+        }
+    }
+</style>
