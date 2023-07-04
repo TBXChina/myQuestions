5fe6490bf0309e329d4ba3b4df4f12a00b5b9139
tangbingxiao <tangbingxiao@foxmail.com>
2023-07-05 00:25:02 +0800

    questions

---
+++ components/choice-item/choice-item.vue
@@ -0,0 +1,43 @@
+<template>
+    <view>
+        <view class="ctx" :class="isActive ? 'active': ''">
+            {{content}}
+        </view>
+
+    </view>
+</template>
+
+<script>
+    export default {
+        name: "choice-item",
+        props: {
+            content: {
+                type: String,
+                default: ''
+            },
+            isActive: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data() {
+            return {
+
+            };
+        }
+    }
+</script>
+
+<style lang="scss">
+    .ctx {
+        font-size: 50rpx;
+        border-radius: 20rpx;
+        background-color: #ffffff;
+        padding: 10rpx;
+        //border: 5px solid red;
+
+        &.active {
+            background-color: #ff0000
+        }
+    }
+</style>
--- pages/questions/questions.vue
+++ pages/questions/questions.vue
@@ -1,19 +1,33 @@
 <template>
     <view class="main">
         <view class="topBlock"></view>
-        <view class="tQuestion">{{questions[questionNo].content}}</view>
+        <view class="tQuestion">{{questionNo + 1}}.{{questions[questionNo].content}}</view>
         <view class="process">
-            <u-line-progress :percentage="30" height="30rpx"></u-line-progress>
+            <u-line-progress :percentage="calPercent()" height="30rpx"></u-line-progress>
         </view>
-        <view class="choices">
-            <choice-list :choices="questions[questionNo].choices"
+        <!--
+        <view class="tChoices">
+            <choice-list :choices="questions[questionNo].choices" :activeNo="ansRecords[questionNo]"
                 @clickChoicEvent="processClickChoicEvent"></choice-list>
         </view>
-        <view class="tPreButton">
-            <u-button type="primary" color="black" text="上一题" @click="preClick"></u-button>
+        -->
+
+        <view class="tChoices">
+            <view v-for="(item, index) in questions[questionNo].choices">
+                <view class="tChoice" @click="pickChoice(index)">
+                    <choice-item :content="String.fromCharCode(65+index) + '.' + item"
+                        :isActive="activeChoice(index)"></choice-item>
+                </view>
+            </view>
         </view>
-        <view class="tPreButton">
-            <u-button type="primary" color="black" text="下一题" @click="nextClick"></u-button>
+        <view class="tQuestionButton">
+            <u-button type="primary" color="black" text="上一题" @click="preQuestion"></u-button>
+        </view>
+        <view v-show="showNextButton()" class="tQuestionButton">
+            <u-button type="primary" color="black" text="下一题" @click="nextQuestion"></u-button>
+        </view>
+        <view v-show="showReportButton()" class="tReportButton">
+            <u-button type="primary" color="black" text="查看报告" @click="clickToReport"></u-button>
         </view>
     </view>
 </template>
@@ -24,7 +38,7 @@
             return {
                 questionNo: 0,
                 questions: [{
-                        content: "默认无边框 dotted: 定义一个点线边框 dashed: 定义一个虚线边框个虚线边框个虚线边框",
+                        content: "虚线边框个虚线边框个虚线边框",
                         choices: [
                             "选项1",
                             "选项2",
@@ -40,23 +54,57 @@
                             "第二个问题选项3",
                             "第二个问题选项4"
                         ]
+                    },
+                    {
+                        content: "第三个问题",
+                        choices: [
+                            "第三个问题选项1",
+                            "第三个问题选项2",
+                            "第三个问题选项3",
+                            "第三个问题选项4"
+                        ]
                     }
-                ]
+                ],
+                ansRecords: []
             };
         },
         methods: {
-            preClick() {
+            preQuestion() {
                 if (this.questionNo > 0) {
                     this.questionNo = this.questionNo - 1;
                 }
             },
-            nextClick() {
-                if (this.questionNo < this.questions.length) {
+            nextQuestion() {
+                if (this.questionNo + 1 < this.questions.length) {
                     this.questionNo = this.questionNo + 1;
                 }
             },
+            activeChoice(index) {
+                return index == this.ansRecords[this.questionNo];
+            },
             processClickChoicEvent(index) {
-                console.log(index)
+                this.ansRecords[this.questionNo] = index;
+                this.$forceUpdate();
+                this.nextQuestion();
+            },
+            pickChoice(index) {
+                this.ansRecords[this.questionNo] = index;
+                this.$forceUpdate();
+                this.nextQuestion()
+            },
+            calPercent() {
+                return Math.floor((this.questionNo + 1) * 100 / this.questions.length);
+            },
+            clickToReport() {
+                uni.navigateTo({
+                    url: "/pages/report/report"
+                })
+            },
+            showNextButton() {
+                return false;
+            },
+            showReportButton() {
+                return (this.questionNo + 1 == this.questions.length) && this.ansRecords[this.questionNo] != null;
             }
         }
     }
@@ -79,23 +127,33 @@
         }
 
         .tQuestion {
+            display: flex;
             margin: 0 auto;
-            text-align: center;
             width: 80%;
+            height: 20%;
             font-size: 60rpx;
             font-family: dingding;
+            align-items: center;
+            justify-content: center;
+            border: 5px solid red;
         }
 
-        .choices {
+        .tChoices {
             margin: 30rpx auto;
             //border: 5px solid red;
             width: 95%;
         }
 
-        .tPreButton {
+        .tChoice {
+            margin: 15rpx auto;
+        }
+
+        .tQuestionButton {
             width: 45%;
             margin: 5rpx 5%;
             //border: 5px solid red;
         }
+
+        .tReportButton {}
     }
 </style>
